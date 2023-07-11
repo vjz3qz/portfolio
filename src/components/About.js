f1171e782fe3ed1d536f39ce09394a7807357662
vjz3qz <vjz3qz@virginia.edu>
2023-07-11 18:59:33 -0400

    created cards for about

--- src/components/About.js
+++ src/components/About.js
@@ -11,46 +11,45 @@ export default function About() {
         </h1>
       </div>
 
-      <div className="container mx-auto lg:max-w-screen-md px-10 pt-5 grid grid-cols-1 sm:grid-cols-3 gap-4 items-center">
-        <div className="flex flex-col items-start sm:mb-16 mb-0 text-center sm:text-left col-span-1 sm:col-span-2">
-          <div>
-            <div>
-              <h1 className="title-font sm:text-2xl text-xl mb-4 font-medium text-white">
-                About Me
-              </h1>
-              <p className="font-light mb-8 leading-relaxed text-slate-200">
+      <div className="container mx-auto lg:max-w-screen-md px-10 pt-5 grid grid-cols-1 md:grid-cols-2 gap-4 items-start">
+        
+        <div className="md:col-span-1">
+          <div className="p-5 my-5 shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl w-full border-4 border-gray-700 bg-gray-800 opacity-80 hover:opacity-90">
+            <h1 className="text-center title-font sm:text-2xl text-xl mb-4 font-medium text-white">
+              About Me
+            </h1>
+            <p className="text-center font-light mb-8 leading-relaxed text-slate-200">
               I'm currently studying Computer Science student at the University of Virginia. 
               After graduation, I aspire to work a Software Engineer in a large metropolitan city. 
               Alongside my passion for technology, I love maintaining an active lifestyle that includes 
               weight lifting, running, hiking, playing chess, and basketball. 
               Beyond my academic pursuits, I also devote my time as a volunteer EMT.
-              </p>
-            </div>
-            <div>
-              <h1 className="title-font sm:text-2xl text-xl mb-4 font-medium text-white">
-                Contacts
-              </h1>
-
-              <p className="font-light mb-8 leading-relaxed text-slate-200">
-                Varun Pasupuleti<br />
-                Ashburn, VA<br />
-                (571) 439-5750<br />
-                vjz3qz@virginia.edu
-              </p>
-            </div>
+            </p>
+          </div>
+          
+          <div className="p-5 my-5 shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl w-full border-4 border-gray-700 bg-gray-800 opacity-80 hover:opacity-90">
+            <h1 className="text-center title-font sm:text-2xl text-xl mb-4 font-medium text-white">
+              Contact Me
+            </h1>
+            <p className="text-center font-light mb-8 leading-relaxed text-slate-200">
+              Varun Pasupuleti<br />
+              Ashburn, VA<br />
+              (571) 439-5750<br />
+              vjz3qz@virginia.edu
+            </p>
           </div>
         </div>
-        <div className="hidden sm:block w-full mx-auto col-span-1 pl-5">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src={`${process.env.PUBLIC_URL}/images/hero.jpg`}
-          />
-        </div>
+
+        <div className="md:col-span-1 p-5 m-5 shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl w-full border-4 border-gray-700 bg-gray-800 opacity-80 hover:opacity-90 sm:block hidden">
+  <img
+    className="object-cover object-center rounded"
+    alt="hero"
+    src={`${process.env.PUBLIC_URL}/images/hero.jpg`}
+  />
+</div>
+
+
       </div>
     </section>
   );
 }
-
-
-
