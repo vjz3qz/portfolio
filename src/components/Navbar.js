afd3ce315ab4bf2e2eb0a5713ff1b4568ad6a0b0
vjz3qz <vjz3qz@virginia.edu>
2023-07-11 19:03:01 -0400

    made navbar links buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <span className="title-font font-light text-white mb-4 md:mb-0">
+          <button>
           <Link to="home"
             smooth={true}
             duration={1000}
@@ -14,9 +15,11 @@ export default function Navbar() {
           >
             Varun Pasupuleti
           </Link>
+          </button>
         </span>
 
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center  text-sm sm:text-base justify-center">
+        <button>
         <Link to="about"
             smooth={true}
             duration={1000}
@@ -25,7 +28,9 @@ export default function Navbar() {
           >
             About
           </Link>
-          <Link to="education"
+        </button>
+        <button>
+        <Link to="education"
             smooth={true}
             duration={1000}
             offset={-90}
@@ -33,26 +38,29 @@ export default function Navbar() {
           >
             Education
           </Link>
-          <Link
-            to="experiences"
+        </button>
+        <button>
+        <Link to="experiences"
             smooth={true}
             duration={1000}
-            offset={-90} 
+            offset={-90}
             className="mr-5 hover:text-white font-light text-slate-300"
           >
             Experiences
           </Link>
-          <Link
-            to="projects"
+        </button>
+        <button>
+        <Link to="projects"
             smooth={true}
             duration={1000}
-            offset={-90} 
+            offset={-90}
             className="mr-5 hover:text-white font-light text-slate-300"
           >
             Projects
           </Link>
-          <Link
-            to="skills"
+        </button>
+        <button>
+        <Link to="skills"
             smooth={true}
             duration={1000}
             offset={-90}
@@ -60,6 +68,7 @@ export default function Navbar() {
           >
             Skills
           </Link>
+        </button>
         </nav>
       </div>
     </header>
