edb0128b1b731e0312341ee63cd47fc185df3f8f
vjz3qz <vjz3qz@virginia.edu>
2023-08-10 02:49:29 -0400

    extracted skills

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,66 +1,27 @@
 import React from "react";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import { experiences } from "../data";
-import { BriefcaseIcon, ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import {
+  BriefcaseIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/solid";
 import { Transition } from "@headlessui/react";
 import { Disclosure } from "@headlessui/react";
 
-function IndividualExperience({ experience }) {
+function Experiences() {
   return (
-    <div className="my-5 shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl w-full border-4 border-gray-700 bg-gray-800 opacity-80 hover:opacity-90">
-
-
-      <Disclosure>
-        {({ open }) => (
-          <>
-            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <div className="flex">
-
-                <div>{experience.logo && (
-                  <img src={`${process.env.PUBLIC_URL}` + experience.logo} alt={`${experience.company} logo`} className="w-10 h-auto pr-2 mr-2 mt-2" />
-                )}</div>
-                <div>
-
-                  <div className="flex items-center">
-                    <h3 className="title-font text-xl text-white mb-2">{experience.company}</h3>
-                  </div>
-                  <div>
-
-                    <p className="text-white mb-2"><span className="font-semibold">{experience.position}</span> •{" "}
-                      <span className="font-light">{experience.time}</span></p>
-                  </div>
-                </div>
-              </div>
-              <div>
-          {open ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+    <section id="experiences">
+      <div>
+        <div className="px-5 mx-auto text-center">
+          <BriefcaseIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            Experiences
+          </h1>
         </div>
-            </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
-              <ul>
-                {experience.description.map((bullet, index) => (
-                  <li key={`${bullet}-${index}`} className="font-light p-1 m-1 leading-relaxed text-slate-200">
-                    • <ReactMarkdown components={{
-                      a: ({ node, ...props }) => <a className="text-green-400 underline" {...props} />,
-                      p: ({ node, ...props }) => <span {...props} />
-                    }}>{bullet}</ReactMarkdown>
-                  </li>
-                ))}
-              </ul>
-            {experience.skills && 
-            (experience.skills.map((skill, index) => (
-              
-            <p key={`${skill}-${index}`}
-              className="p-1 m-1 text-sm text-white inline-flex font-light border-1 focus:outline-none bg-gray-600 hover:text-white rounded"
-            >
-              {skill}
-            </p>
-            )))
-            }
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </div>
+        <ExperienceList experiences={experiences} />
+      </div>
+    </section>
   );
 }
 
@@ -88,20 +49,112 @@ function ExperienceList({ experiences }) {
   );
 }
 
-function Experiences() {
+function IndividualExperience({ experience }) {
+  const { logo, company, position, time, description, skills } = experience;
   return (
-    <section id="experiences">
-      <div>
-        <div className="px-5 mx-auto text-center">
-          <BriefcaseIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Experiences
-          </h1>
-        </div>
-        <ExperienceList experiences={experiences} />
-      </div>
-    </section>
+    <div className="my-5 shadow-lg transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl w-full border-4 border-gray-700 bg-gray-800 opacity-80 hover:opacity-90">
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <div className="flex">
+                <div>
+                  {logo && (
+                    <img
+                      src={`${process.env.PUBLIC_URL}` + logo}
+                      alt={`${company} logo`}
+                      className="w-10 h-auto pr-2 mr-2 mt-2"
+                    />
+                  )}
+                </div>
+                <div>
+                  <div className="flex items-center">
+                    <h3 className="title-font text-xl text-white mb-2">
+                      {company}
+                    </h3>
+                  </div>
+                  <div>
+                    <p className="text-white mb-2">
+                      <span className="font-semibold">
+                        {position}
+                      </span>{" "}
+                      • <span className="font-light">{time}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                {open ? (
+                  <ChevronUpIcon className="h-6 w-6" />
+                ) : (
+                  <ChevronDownIcon className="h-6 w-6" />
+                )}
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              <ul>
+                {description.map((bullet, index) => (
+                  <li
+                    key={`${bullet}-${index}`}
+                    className="font-light p-1 m-1 leading-relaxed text-slate-200"
+                  >
+                    •{" "}
+                    <ReactMarkdown
+                      components={{
+                        a: ({ node, ...props }) => (
+                          <a className="text-green-400 underline" {...props} />
+                        ),
+                        p: ({ node, ...props }) => <span {...props} />,
+                      }}
+                    >
+                      {bullet}
+                    </ReactMarkdown>
+                  </li>
+                ))}
+              </ul>
+              {skills && <ExperienceSkills skills={skills} />}
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
   );
+}
+
+function ExperienceDescription({ description }) {
+  return (
+    <ul>
+      {description.map((bullet, index) => (
+        <li
+          key={`${bullet}-${index}`}
+          className="font-light p-1 m-1 leading-relaxed text-slate-200"
+        >
+          •{" "}
+          <ReactMarkdown
+            components={{
+              a: ({ node, ...props }) => (
+                <a className="text-green-400 underline" {...props} />
+              ),
+              p: ({ node, ...props }) => <span {...props} />,
+            }}
+          >
+            {bullet}
+          </ReactMarkdown>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function ExperienceSkills({ skills }) {
+  return skills.map((skill, index) => (
+    <p
+      key={`${skill}-${index}`}
+      className="p-1 m-1 text-sm text-white inline-flex font-light border-1 focus:outline-none bg-gray-600 hover:text-white rounded"
+    >
+      {skill}
+    </p>
+  ));
 }
 
 export default Experiences;
