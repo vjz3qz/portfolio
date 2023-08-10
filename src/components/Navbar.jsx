93c5d34c8076dc7948a7cf7aea0b6516aa845056
vjz3qz <vjz3qz@virginia.edu>
2023-08-10 02:23:12 -0400

    cleaned navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
 export default function Navbar() {
   return (
@@ -7,72 +7,33 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <span className="title-font font-light text-white mb-4 md:mb-0">
           <button>
-          <Link to="home"
-            smooth={true}
-            duration={1000}
-            offset={-75}
-            className="ml-3 text-xl"
-          >
-            Varun Pasupuleti
-          </Link>
+            <Link
+              to="home"
+              smooth={true}
+              duration={1000}
+              offset={-75}
+              className="ml-3 text-xl"
+            >
+              Varun Pasupuleti
+            </Link>
           </button>
         </span>
 
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center  text-sm sm:text-base justify-center">
-        <button>
-        <Link to="about"
-            smooth={true}
-            duration={1000}
-            offset={-90}
-            className="mr-5 hover:text-white font-light text-slate-200"
-          >
-            About
-          </Link>
-        </button>
-        <button>
-        <Link to="education"
-            smooth={true}
-            duration={1000}
-            offset={-90}
-            className="mr-5 hover:text-white font-light text-slate-200"
-          >
-            Education
-          </Link>
-        </button>
-        <button>
-        <Link to="experiences"
-            smooth={true}
-            duration={1000}
-            offset={-90}
-            className="mr-5 hover:text-white font-light text-slate-200"
-          >
-            Experiences
-          </Link>
-        </button>
-        <button>
-        <Link to="projects"
-            smooth={true}
-            duration={1000}
-            offset={-90}
-            className="mr-5 hover:text-white font-light text-slate-200"
-          >
-            Projects
-          </Link>
-        </button>
-        <button>
-        <Link to="skills"
-            smooth={true}
-            duration={1000}
-            offset={-90}
-            className="mr-5 hover:text-white font-light text-slate-200"
-          >
-            Skills
-          </Link>
-        </button>
+          {[['About', 'about'], ['Education', 'education'], ['Experiences', 'experiences'], ['Projects', 'projects'], ['Skills', 'skills']].map(([text, id]) => (
+            <button>
+              <Link
+                to={id}
+                smooth={true}
+                duration={1000}
+                offset={-90}
+                className="mr-5 hover:text-white font-light text-slate-200"
+              >
+                {text}
+              </Link>
+            </button>))}
         </nav>
       </div>
     </header>
   );
 }
-
-
