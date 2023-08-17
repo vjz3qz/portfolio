4c2b9407beea2d4b28efecf616a2f5c85b5e7e78
vjz3qz <vjz3qz@virginia.edu>
2023-08-17 00:54:25 -0400

    added tailwind styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,9 @@ export default function Home() {
   return (
     <section
       id="home"
+      className="bg-center bg-cover w-screen h-screen relative"
       style={{
         backgroundImage: `url(${process.env.PUBLIC_URL}/images/backdrop.jpg)`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "100vh",
-        position: "relative",
       }}
     >
       <div className="container mx-auto flex px-10 py-20 md:py-36 lg:py-48 md:flex-row flex-col items-center animate-fadeIn">
@@ -19,7 +14,7 @@ export default function Home() {
           <h1 className="title-font text-5xl sm:text-6xl md:text-7xl mb-4 font-medium text-white">
             I'm Varun Pasupuleti.
           </h1>
-          <p className=" text-md md:text-lg mb-8 leading-relaxed font-light text-white">
+          <p className="text-md md:text-lg mb-8 leading-relaxed font-light text-white">
             I'm a <span className="font-normal">student</span> and{" "}
             <span className="font-normal">software engineer</span> who is deeply
             passionate about my work. I love tackling complex problems, even
@@ -30,23 +25,11 @@ export default function Home() {
           </p>
           <div className="flex justify-center">
             {[
-              [
-                "https://github.com/vjz3qz",
-                "GitHub",
-                "inline-flex text-white bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg",
-              ],
-              [
-                "https://www.linkedin.com/in/varunpasupuleti/",
-                "LinkedIn",
-                "ml-4 inline-flex text-white bg-sky-700 border-0 py-2 px-6 focus:outline-none hover:bg-sky-800 hover:text-white rounded text-lg",
-              ],
-              [
-                "https://drive.google.com/file/d/1qWdpYmH_BkTwdyRTWrV8SeYr2zgvDac6/view?usp=sharing",
-                "Resume",
-                "ml-4 inline-flex text-white bg-gray-700 border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 hover:text-white rounded text-lg",
-              ],
+              ["https://github.com/vjz3qz", "GitHub", "bg-green-600 hover:bg-green-700"],
+              ["https://www.linkedin.com/in/varunpasupuleti/", "LinkedIn", "bg-sky-700 hover:bg-sky-800 hover:text-white ml-4"],
+              ["https://drive.google.com/file/d/1qWdpYmH_BkTwdyRTWrV8SeYr2zgvDac6/view?usp=sharing", "Resume", "bg-gray-700 hover:bg-gray-800 hover:text-white ml-4"],
             ].map(([link, text, style]) => (
-              <a href={link} className={style}>
+              <a href={link} className={`inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg ${style}`}>
                 {text}
               </a>
             ))}
