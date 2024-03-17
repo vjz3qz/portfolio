3f2f43923e6ec87448c8571e996516901e89a73e
vjz3qz <vjz3qz@virginia.edu>
2024-03-17 17:58:22 -0400

    added bronco and open link in new tab

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -59,6 +59,8 @@ class Intro extends React.Component {
                 ].map(([link, text, style]) => (
                   <a
                     href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className={`inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg ${style}`}
                   >
                     {text}
