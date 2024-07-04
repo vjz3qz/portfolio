106e0333c8a19e7dd9cacdf136b3f8064e7237dd
vjz3qz <vjz3qz@virginia.edu>
2024-07-03 22:34:39 -0700

    gray

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="text-gray-400 bg-neutral-900 body-font">
       <Navbar />
       <Intro />
       <TransparentRule />
