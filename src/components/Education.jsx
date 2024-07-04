a74c3283ead3fea0ff70823cd8fd20dba921e807
vjz3qz <vjz3qz@virginia.edu>
2024-07-04 00:44:52 -0700

    chore: update spacing and alignment in Education and Experiences components

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -103,8 +103,8 @@ function Education() {
     <section id="education">
       <div>
         <div className="px-5 mx-auto text-center">
-          <div className="mx-auto inline-block w-10 mb-6" />
-          {/* <BriefcaseIcon className="mx-auto inline-block w-10 mb-6" /> */}
+          <div className="mx-auto inline-block w-10 mb-8" />
+          {/* <BriefcaseIcon className="mx-auto inline-block w-10 mb-8" /> */}
           <h1 className="sm:text-4xl text-3xl font-medium  mb-6 text-white">
             Education
           </h1>
